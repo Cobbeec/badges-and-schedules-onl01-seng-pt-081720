e0f9896f58046380d43984e9a761235c221d7d1a
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-23 22:05:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
 def badge_maker (names)
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  array.each {|name| puts "Hello, my name is #{name}."}
+  array.each {|names| puts "Hello, my name is #{names}."}
 end 
