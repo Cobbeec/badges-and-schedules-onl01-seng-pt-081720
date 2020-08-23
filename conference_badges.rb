3843c1b174bf0f6fc73ad4d1927a25cc06340015
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-23 22:22:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,11 @@ def badge_maker (name)
   return "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator (array)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  speakers.each {|speaker| puts "Hello, my name is #{speaker}"}
-end 
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
+end
   
