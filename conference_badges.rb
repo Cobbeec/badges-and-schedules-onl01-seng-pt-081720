096798902d821c27c2322932b59dab1745baa9b4
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-23 22:09:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker (names)
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker (name)
+  return 
   names.each {|names| puts "Hello, my name is #{names}."}
 end 
